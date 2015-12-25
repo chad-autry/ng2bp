f9438a3b10e50262cbbffacebcc6182019dfd115
Chad Autry <chad.autry@gmail.com>
2015-12-24 22:00:40 -0600

    Mess with the jumbotron content some more Trimmed down the list of primary 3rd party components to only include those actually used

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -7,19 +7,19 @@ module.exports = ngCore
         template: `
         <div class="container">
 <div class="jumbotron">
-  <h1 class="text-center">Special Notices</h1>
+  <h1 class="text-center">Notice</h1>
   <ul class="lead">
     <li>
-      <i class="fa fa-exclamation-triangle"></i>&nbsp;Angular 2 is still in beta. As such the plain JavaScript documentation is currentlly lacking and the general recommendation at the moment is to use TypeScript for better support.
+      <i class="fa fa-exclamation-triangle"></i>&nbsp;Angular 2 is still in beta. On top of any normal beta issues, another issue is plain JavaScript documentation and tutorials are extremely limited. A new user of Angular 2 is highly encouraged to go the TypeScript route rather than this project's pure JS route.
     </li>
     <li>
-      ng2bp has its origins in <a href="https://github.com/joshdmiller"><i class="fa fa-at"></i>joshdmiller's</a>&nbsp;<a href="https://github.com/ngbp/ngbp"><i class="fa fa-code-fork"></i>ngbp project</a>
-      . It has since then been heavily modified until it is only superficially similar.
+      <i class="fa fa-code-fork"></i>&nbsp;ng2bp has its origins in <a href="https://github.com/joshdmiller">joshdmiller's</a>&nbsp;<a href="https://github.com/ngbp/ngbp">ngbp project</a>
+      . It has since then been heavily modified.
     </li>
     <li>
-      Special thanks to everyone over in the <a href="https://gitter.im/angular/angular">
-      Angular 2 gitter chat&nbsp;<i class="fa fa-comments"></i></a>. Particularlly <a href="https://github.com/brandonroberts">
-      <i class="fa fa-at"></i>brandonroberts</a> for his helpful plunkers.
+      <i class="fa fa-comments"></i>&nbsp;Special thanks to everyone over in the <a href="https://gitter.im/angular/angular">
+      Angular 2 gitter chat&nbsp;</a>. Particularlly <a href="https://github.com/brandonroberts">
+      brandonroberts</a> for his helpful plunkers.
     </li>
   </ul>
 </div>
@@ -39,15 +39,6 @@ module.exports = ngCore
       </p>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4">
-      <h4><i class="fa fa-retweet"></i> Modularization</h4>
-      <p>
-        Supports a structure that maintains separation of concerns while
-        ensuring maximum code reuse.
-      </p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-4">
       <h4><i class="fa fa-star"></i> AngularJS 2</h4>
       <p>
         JavaScript framework that augments browser-based, single-page
@@ -55,31 +46,8 @@ module.exports = ngCore
         <a href="https://angular.io/">More &raquo;</a>
       </p>
     </div>
-    <div class="col-xs-12 col-sm-6 col-md-4">
-      <h4><i class="fa fa-resize-small"></i> LESS CSS</h4>
-      <p>
-        The dynamic stylesheet language that extends CSS with efficiency.
-        <a href="http://lesscss.org">More &raquo;</a>
-      </p>
-    </div>
-    <div class="col-xs-12 col-sm-6 col-md-4">
-      <h4><i class="fa fa-twitter"></i> Twitter Bootstrap</h4>
-      <p>
-        Sleek, intuitive, and powerful front-end framework for faster and easier
-        web development.
-        <a href="http://getbootstrap.com">More &raquo;</a>
-      </p>
-    </div>
   </div>
   <div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-4">
-      <h4><i class="fa fa-circle"></i> Angular UI Bootstrap</h4>
-      <p>
-        Pure AngularJS components for Bootstrap written by the
-        <a href="https://github.com/angular-ui?tab=members">AngularUI Team</a>.
-        <a href="http://angular-ui.github.com/bootstrap">More &raquo;</a>
-      </p>
-    </div>
     <div class="col-xs-12 col-sm-6 col-md-4">
       <h4><i class="fa fa-flag"></i> Font Awesome</h4>
       <p>
@@ -90,13 +58,18 @@ module.exports = ngCore
       </p>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4">
-      <h4><i class="fa fa-asterisk"></i> Placeholders</h4>
+      <h4><i class="fa fa-twitter"></i> Twitter Bootstrap</h4>
       <p>
-        Client-side image and text placeholder directives written in pure
-        AngularJS to make designing mock-ups wicked-fast.
-        <a href="http://joshdmiller.github.com/angular-placeholders">
-          More &raquo;
-        </a>
+        Sleek, intuitive, and powerful front-end framework for faster and easier
+        web development.
+        <a href="http://getbootstrap.com">More &raquo;</a>
+      </p>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+      <h4><i class="fa fa-resize-small"></i> LESS CSS</h4>
+      <p>
+        The dynamic stylesheet language that extends CSS with efficiency.
+        <a href="http://lesscss.org">More &raquo;</a>
       </p>
     </div>
   </div>
